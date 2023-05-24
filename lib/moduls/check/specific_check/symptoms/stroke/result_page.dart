58540364513a0a3b53501774f54ce7354b32ddee
sdcxzsadq@gmail.com <sdcxzsadq@gmail.com>
2023-05-24 19:08:09 +0300

    Show Results

--- lib/moduls/check/specific_check/symptoms/stroke/result_page.dart
+++ lib/moduls/check/specific_check/symptoms/stroke/result_page.dart
@@ -1,4 +1,5 @@
 import 'package:diseases_checker/moduls/home/homepage.dart';
+import 'package:diseases_checker/shared/components/constants.dart';
 import 'package:flutter/material.dart';
 
 class StrokeResultPage extends StatelessWidget {
@@ -33,10 +34,11 @@ class StrokeResultPage extends StatelessWidget {
         title: const Text('Results'),
       ),
       body: ListView.builder(
-        itemCount: selectedSymptoms.length,
+        itemCount: BspcList.length,
         itemBuilder: (context, index) {
           return ListTile(
-            title: Text(selectedSymptoms[index]),
+            title: Text(BspcList[index].name),
+            trailing: Text(BspcList[index].value),
           );
         },
       ),
